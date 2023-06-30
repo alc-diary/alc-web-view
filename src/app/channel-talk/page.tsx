a1a915156e100543db2cba54e2fdf2eee0907aa3
itssweetrain <artraine@gmail.com>
2023-07-01 02:15:39 +0900

    initial

---
+++ src/app/channel-talk/page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next" 
+import styled from "styled-components";
+
+const ChannelTalk: NextPage = () => {
+    return (
+        <Text>채널톡</Text>
+    )  
+}
+
+export default ChannelTalk
+
+const Text = styled.div`
+    color: #ff3033;
+`
