cd1df5e5d7557a521f54c4e433663dedb24ea8c3
itssweetrain <artraine@gmail.com>
2023-07-02 01:58:54 +0900

    env 수정

--- src/service/channel-talk/ChannelTalkView.tsx
+++ src/service/channel-talk/ChannelTalkView.tsx
@@ -18,3 +18,16 @@ const ChannelTalk: React.FC = () => {
 }
 
 export default ChannelTalk;
+
+declare global {
+    interface Window {
+      ChannelIO?: IChannelIO;
+      ChannelIOInitialized?: boolean;
+    }
+  }
+  
+  interface IChannelIO {
+    c?: (...args: any) => void;
+    q?: [methodName: string, ...args: any[]][];
+    (...args: any): void;
+  }
